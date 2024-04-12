2ab076d8053333488f5bcbb3fcfe4b2346c38fa0
Britnay Gore <britnaygore268@gmail.com>
2024-04-11 22:29:04 -0400

    Can console warn clicked buttons

--- components/Calculator.js
+++ components/Calculator.js
@@ -1,30 +1,27 @@
 import { Button } from 'react-bootstrap';
-import React from 'react';
+// import React, { useState } from 'react';
 import CalcStyles from '../styles/Calculator.module.css';
 // import PropTypes from 'prop-types';
 
 export default function Calculator() {
+  // const [value, setValue] = useState('');
+  const handleButtonClick = (btn) => {
+    console.warn(`Button clicked: ${btn}`);
+  };
+
   const btnText = [
     ['C', '^', '%', '/'],
     [7, 8, 9, 'x'],
     [4, 5, 6, '-'],
     [1, 2, 3, '+'],
-    [0, ' ', '.', '='],
+    [0, '.', '='],
   ];
 
   return (
     <div className={CalcStyles.gridBtn}>
       {btnText.flat().map((btn) => (
-        <Button key={btn}>{btn}</Button>
+        <Button className={`${CalcStyles.buttonColor}`} key={btn} onClick={() => handleButtonClick(btn)}>{btn}</Button>
       ))}
     </div>
   );
 }
-
-// Calculator.propTypes = {
-//   value: PropTypes.number,
-// };
-
-// Calculator.defaultProps = {
-//   value: 0,
-// };
--- components/InputField.js
+++ components/InputField.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Form } from 'react-bootstrap';
+// import PropTypes from 'prop-types';
 
 export default function InputField() {
   return (
